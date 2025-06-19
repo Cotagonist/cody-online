49756558deb885d5a2a67df17e24553e53e95d3f
Cody Musser <cody+accounts@codymusser.com>
2025-06-19 14:31:16 -0400

    Enable static site export in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // 👈 this enables static site export for Cloudflare Pages
 };
 
 export default nextConfig;
